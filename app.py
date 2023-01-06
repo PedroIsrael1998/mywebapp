638ece0eabd07116f171f7f77055811aae4e2229
PedroIsrael1998 <106317168+PedroIsrael1998@users.noreply.github.com>
2023-01-06 10:45:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,55 @@
-import dash
-from dash import html, dcc
-import dash_bootstrap_components as dbc
-import pandas as pd
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
 
-app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SPACELAB])
-server = app.server
+df = px.data.gapminder()
 
-sidebar = dbc.Nav(
-            [
-                dbc.NavLink(
-                    [
-                        html.Div(page["name"], className="ms-2"),
-                    ],
-                    href=page["path"],
-                    active="exact",
-                )
-                for page in dash.page_registry.values()
-            ],
-            vertical=True,
-            pills=True,
-            className="bg-light",
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.H4("Animated GDP and population over decades"),
+        html.P("Select an animation:"),
+        dcc.RadioItems(
+            id="selection",
+            options=["GDP - Scatter", "Population - Bar"],
+            value="GDP - Scatter",
+        ),
+        dcc.Loading(dcc.Graph(id="graph"), type="cube"),
+    ]
 )
 
-app.layout = dbc.Container([
-    #title:
-    dbc.Row([
-        # color blue
-        html.Div(children="My projects",style={'fontSize':50, 'textAlign':'center', 'color':'#1E90FF'})
-    ]),
-    # line break:
-    html.Hr(),
 
-    dbc.Row(
-        [
-            #sidebar
-            dbc.Col(
-                [
-                    sidebar
-                ], xs=4, sm=4, md=2, lg=2, xl=2, xxl=2),
-            #page_container:
-            dbc.Col(
-                [
-                    dash.page_container
-                ], xs=8, sm=8, md=10, lg=10, xl=10, xxl=10)
-        ]
-    )
-], fluid=True)
+@app.callback(
+    Output("graph", "figure"), Input("selection", "value")
+)
+def display_animated_graph(selection):
+    animations = {
+        "GDP - Scatter": px.scatter(
+            df,
+            x="gdpPercap",
+            y="lifeExp",
+            animation_frame="year",
+            animation_group="country",
+            size="pop",
+            color="continent",
+            hover_name="country",
+            log_x=True,
+            size_max=55,
+            range_x=[100, 100000],
+            range_y=[25, 90],
+        ),
+        "Population - Bar": px.bar(
+            df,
+            x="continent",
+            y="pop",
+            color="continent",
+            animation_frame="year",
+            animation_group="country",
+            range_y=[0, 4000000000],
+        ),
+    }
+    return animations[selection]
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run_server(debug=True)
